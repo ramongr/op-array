2d80f21883c030efced8de9f6d7ee02ec4cca993
Ramon Rodrigues <cerberus.ramon@gmail.com>
2021-09-26 17:16:21 +0100

    Added array properties up to the tenth position

--- operations/positional.js
+++ operations/positional.js
@@ -17,6 +17,48 @@ Object.defineProperty(Array.prototype, 'third', {
   },
 });
 
+Object.defineProperty(Array.prototype, 'fourth', {
+  get: function() {
+    return this[3];
+  },
+});
+
+Object.defineProperty(Array.prototype, 'fifth', {
+  get: function() {
+    return this[4];
+  },
+});
+
+Object.defineProperty(Array.prototype, 'sixth', {
+  get: function() {
+    return this[5];
+  },
+});
+
+Object.defineProperty(Array.prototype, 'seventh', {
+  get: function() {
+    return this[6];
+  },
+});
+
+Object.defineProperty(Array.prototype, 'eighth', {
+  get: function() {
+    return this[7];
+  },
+});
+
+Object.defineProperty(Array.prototype, 'nineth', {
+  get: function() {
+    return this[8];
+  },
+});
+
+Object.defineProperty(Array.prototype, 'tenth', {
+  get: function() {
+    return this[9];
+  },
+});
+
 Object.defineProperty(Array.prototype, 'last', {
   get: function() {
     return this[this.length - 1];
